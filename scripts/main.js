92b12ce4a8ef88c5f42a4211c292681b6c1dfaa5
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-02 15:12:57 -0400

    cleaning up comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,12 @@
 import { Navbar } from "./navbar/Navbar.js";
-// import { Form } from "./form/Form.js";
 import { FormSelect } from "./form/FormSelect.js";
 // import {getFiveDayWeather} from "./weather/WeatherProvider.js"
-// import { EaterySelect } from "./eateries/EateriesSelect.js";
-// import {eateriesList} from "./eateries/EateriesList.js"
-// import { AttractionSelect } from "./attractions/AttractionSelect.js";
-// import { parkSelect } from "./parks/ParkSelect.js";
-// import { getPark } from "./parks/ParkProvider.js";
-// import { parkList } from "./parks/ParkList.js";
 import {Footer} from "./footer/Footer.js"
 
 
 
 Navbar();
 FormSelect();
-// EaterySelect()
-// AttractionSelect()
-// getPark()
-// parkSelect()
-// parkList()
 Footer();
 
 

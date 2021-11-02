e76ca221247c81b2de305a4a7d58907df764a517
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-02 10:54:34 -0400

    commenting out render functions before team pull requests

--- scripts/form/FormSelect.js
+++ scripts/form/FormSelect.js
@@ -22,44 +22,44 @@ export const FormSelect = () => {
     // })
 }
 
-const renderParks = (parksCollection) => {
-    contentTarget.innerHTML = `
-        <select class="form-control dropdown" id="">
-            <option selected>Choose...</option>
-            ${
-                parksCollection.map((park) => {
-                    const parkName = park.name;
-                    return `<option>${parkName}</option>`
-                })
-            }
-        </select>
-    `
-}
+// const renderParks = (parksCollection) => {
+//     contentTarget.innerHTML = `
+//         <select class="form-control dropdown" id="">
+//             <option selected>Choose...</option>
+//             ${
+//                 parksCollection.map((park) => {
+//                     const parkName = park.name;
+//                     return `<option>${parkName}</option>`
+//                 })
+//             }
+//         </select>
+//     `
+// }
 
-const renderBizarraries = (bizarrariesCollection) => {
-    contentTarget.innerHTML = `
-        <select class="form-control dropdown" id="">
-            <option selected>Choose...</option>
-            ${
-                bizarrariesCollection.map((attraction) => {
-                    const attractionName = attraction.name;
-                    return `<option>${attractionName}</option>`
-                })
-            }
-        </select>
-    `
-}
+// const renderBizarraries = (bizarrariesCollection) => {
+//     contentTarget.innerHTML = `
+//         <select class="form-control dropdown" id="">
+//             <option selected>Choose...</option>
+//             ${
+//                 bizarrariesCollection.map((attraction) => {
+//                     const attractionName = attraction.name;
+//                     return `<option>${attractionName}</option>`
+//                 })
+//             }
+//         </select>
+//     `
+// }
 
-const renderEateries = (eateriesCollection) => {
-    contentTarget.innerHTML = `
-        <select class="form-control dropdown" id="">
-            <option selected>Choose...</option>
-            ${
-                eateriesCollection.map((eatery) => {
-                    const eateryName = eatery.name;
-                    return `<option>${eateryName}</option>`
-                })
-            }
-        </select>
-    `
-}
+// const renderEateries = (eateriesCollection) => {
+//     contentTarget.innerHTML = `
+//         <select class="form-control dropdown" id="">
+//             <option selected>Choose...</option>
+//             ${
+//                 eateriesCollection.map((eatery) => {
+//                     const eateryName = eatery.name;
+//                     return `<option>${eateryName}</option>`
+//                 })
+//             }
+//         </select>
+//     `
+// }

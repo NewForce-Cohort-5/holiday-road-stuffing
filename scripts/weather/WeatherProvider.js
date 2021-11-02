1741d44ad5e0aeaaadda6eeca9362746c0bf0c20
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-02 09:45:41 -0400

    wweather api parameters now changed to geographic coordinates from city name

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,13 +1,18 @@
+let fiveDayWeatherForecast = [];
+
+export const useWeather = () => {
+    return fiveDayWeatherForecast.slice();
+}
 
 
-
-export const getWeather = (cityName) => {
-    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=af3c1ecfd14387178ba1ebf6216c79c0`)
+export const getFiveDayWeather = (lat,lon) => {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=af3c1ecfd14387178ba1ebf6216c79c0`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data);
+        fiveDayWeatherForecast = data;
     })
     .catch(e => {
-        alert('Wrong city name')
+        alert('Please check latitude and longitude coordinates');
     })
 }

3aa4e12b6942fc3c47ae6358c32c1cf2dc9f8317
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-11-04 13:41:28 -0400

    width changed on form and itinerary-container

--- scripts/weather/WeatherCard.js
+++ scripts/weather/WeatherCard.js
@@ -138,7 +138,7 @@ export const weatherCard = (weatherObj) => {
   </div>
 </div>
     `
-  } else if (weatherDescriptionToCompare.includes("clear")) {
+  } else if (weatherDescriptionToCompare.includes("clear sky")) {
     return `
 <div class="accordion" id="accordionExample">
   <div class="accordion-item">
@@ -166,32 +166,5 @@ export const weatherCard = (weatherObj) => {
 </div>
     `
   }
-
-
-
-    return `
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        ${new Date(weatherObj.dt_txt).toLocaleDateString('en-US')} 
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <div class="card">
-        <div class="card-body">
-            <p class="card-text">${(weatherObj.weather[0].description).toUpperCase()}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Temp: ${Math.round(weatherObj.main.temp)}&#8457;</li>
-            <li class="list-group-item">Humidity: ${weatherObj.main.humidity}%</li>
-            <li class="list-group-item">Wind: ${Math.round(weatherObj.wind.speed)}mph</li>
-        </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-    `
+  
 }
